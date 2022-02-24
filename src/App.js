d53af880ed63ef1576234e8b8b1937042d2098b6
Jeleen Limawan <je0110@uw.edu>
2022-02-23 21:24:14 -0800

    more songs in database, added footer in App()

--- src/App.js
+++ src/App.js
@@ -3,7 +3,17 @@ import React, { useState } from 'react'; //import React Component
 function App() {
   console.log("hello world");
 
-  return "Hello world";
+  return (
+    
+
+    <footer>
+      <div className="container">
+      <p><a href='#'></a>About</p>
+      <p><a href="mailto:groovespace@groove.uw.edu"><span className="material-icons">email</span>groovespace@groove.uw.edu</a></p>
+      <p><a href="tel:123-456-7890"><span className="material-icons">phone</span>123-456-7890</a></p>
+      </div>
+    </footer>
+  );
 }
 
 export default App;
