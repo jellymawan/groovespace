b5730b074b51a2a14ba6b1fe211425cb23726733
Jaehoon Choi <jaehoonchoi@Jaehoons-MacBook-Pro.local>
2022-02-28 14:30:42 -0800

    change css

--- src/App.js
+++ src/App.js
@@ -4,31 +4,29 @@ import {CardList} from './CardList.js';
 import HeaderNav from './HeaderNav';
 import MusicPage from './MusicPage';
 import {MusicDetail} from './MusicDetail';
-import {Link, Routes, Route, Navigate} from 'react-router-dom';
-
-
-
+import {Link, Routes, Route, Outlet} from 'react-router-dom';
+ 
+ 
+ 
 function App(props) {
-  return (
-    <div>
-      <HeaderNav />
-      <div>
-        <Routes>
-          <Route path="/about" element={<Static.AboutPage />}/>
-          <Route path="/songs" element={<MusicPage />}>
-            <Route path=":songID" element={<MusicDetail songs={props.songs}/>} />
-            <Route index="/songs" element={<CardList songs={props.songs}/>} />
-          </Route>
-          <Route path="/" element={<Navigate to="/songs"/>}/>
-          <Route path="*" element={<Static.ErrorPage />} />
-        </Routes>
-      </div>
-      <div>
-        <Static.Footer />
-      </div>
-    </div>
-  );
+ return (
+   <div>
+     <HeaderNav />
+     <div>
+       <Routes>
+           <Route path="/home" element={<MusicPage />}>
+             <Route path=":songID" element={<MusicDetail songs={props.songs}/>} />
+             <Route index="/home" element={<CardList songs={props.songs}/>} />
+           </Route>
+           <Route path="/about" element={<Static.AboutPage />}/>
+           <Route path="*" element={<Static.ErrorPage />} />
+         </Routes>
+     </div>
+     <div>
+       <Static.Footer />
+     </div>
+   </div>
+ );
 }
-
-
+ 
 export default App;
