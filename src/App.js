645b4b5fd894d3ffac43e1718ff9f7bf2054b89c
Benson Luu <benluu@uw.edu>
2022-03-09 18:19:34 -0800

    Maybe the sign in is done, but I actually doubt it

--- src/App.js
+++ src/App.js
@@ -1,34 +1,55 @@
-import React, { useState } from 'react'; //import React Component
+import React, { useEffect, useState } from 'react'; //import React Component
 import * as Static from './StaticPages';
-import {CardList} from './CardList.js';
+import { CardList } from './CardList.js';
 import HeaderNav from './HeaderNav';
 import MusicPage from './MusicPage';
-import {MusicDetail} from './MusicDetail';
-import {Link, Routes, Route, Outlet} from 'react-router-dom';
-import {Browse} from './Browse';
- 
- 
+import { MusicDetail } from './MusicDetail';
+import { Link, Routes, Route, Outlet, Navigate } from 'react-router-dom';
+import { Browse } from './Browse';
+import { MdOpenInNewOff, MdSettingsInputComponent } from 'react-icons/md';
+import { useAuthState } from 'react-firebase-hooks/auth';
+import { getAuth } from 'firebase/auth';
+
+
+
+const [user, loading, error] = useAuthState(getAuth());
+
+const currentUser = user;
+
 function App(props) {
- return (
-  <div>
-    <HeaderNav />
+  return (
     <div>
-      <Routes>
-        <Route path="/" element={<CardList songs={props.songs}/>}/>
+      <HeaderNav />
+      <div>
+        <Routes>
+          <Route path="/" element={<CardList songs={props.songs} />} />
           <Route path="/songs" element={<MusicPage />}>
-            <Route path=":songID" element={<MusicDetail songs={props.songs}/>} />
-            <Route index="/songs" element={<CardList songs={props.songs}/>} />
+            <Route path=":songID" element={<MusicDetail songs={props.songs} />} />
+            <Route index="/songs" element={<CardList songs={props.songs} />} />
           </Route>
-          <Route path="/browse" element={<Browse songs={props.songs}/>}/>
-          <Route path="/about" element={<Static.AboutPage />}/>
+          <Route path="/browse" element={<Browse songs={props.songs} />} />
+          <Route path="/about" element={<Static.AboutPage />} />
+          <Route path="/signin" element={<SignInPage user={currentUser} loginFunction={loginUser} />} />
           <Route path="*" element={<Static.ErrorPage />} />
         </Routes>
+      </div>
+      <div>
+        <Static.Footer />
+      </div>
     </div>
-    <div>
-      <Static.Footer />
-    </div>
-  </div>
- );
-} 
+  );
+}
 
 export default App;
+
+function ProtectedPage(props) {
+  if (props.loading) {
+    return null;
+  }
+  else if (!props.user) {
+    return <Navigate to="/signin" />
+  }
+  else {
+    return <Outlet />
+  }
+}
