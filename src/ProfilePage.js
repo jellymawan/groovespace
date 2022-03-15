5b9cb6d807a17116da9a27d9486bab87cfa5599b
Benson Luu <benluu@uw.edu>
2022-03-15 03:22:06 -0700

    I hate github

--- src/ProfilePage.js
+++ src/ProfilePage.js
@@ -1,6 +1,8 @@
 import React, { useEffect } from 'react';
 
 export function ProfilePage(props) {
-
-    
+    const displayName = props.user ? props.user.displayName : null;
+    return (
+        <h1>{displayName}</h1>
+    )
 }
