81faaed98fbc2c8e0d7836595cfd41cbd248beaf
Benson Luu <benluu@uw.edu>
2022-03-15 01:32:18 -0700

    Delete

--- src/ProfilePage.js
+++ src/ProfilePage.js
@@ -2,15 +2,5 @@ import React, { useEffect } from 'react';
 
 export function ProfilePage(props) {
 
-    let results = props.songs.filter((song) => {
-        //console.log(props.user.displayName == song.artist);
-        return (props.user.displayName == song.artist);
-    });
-
-    console.log(results);
-
-    return (
-    <h1>{props.user.displayName}</h1>
-    )
+    
 }
-  
